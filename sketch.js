84930862ac1eb2ea6d4600fec87696bde86158ab
Asim Satti <aks618@nyu.edu>
2022-10-10 00:42:34 -0700

    Init take on grabbing avg pixel values and assiging it to a kernelsizes W x H

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 
 var originalVideo;
 var pg;
+var kernel=[25,100];
+var kernelSize=kernel[0]*kernel[1];
+var pixToEdit=[];
 var capture;
+var Kcount=0;
 var w = 640;
 var h = 480;
 var sinx=0.000;
@@ -30,49 +34,94 @@ function setup() {
 
 // [r g b a] r g b a r g b a ...
 function draw() {
+
     // pg.background(100);
    //image(capture, 0, 0, w, h);
      capture.loadPixels();
      var NewPix=capture.pixels;
    //  originalVideo.loadPixels();
 
+
+   //Filter
+
+
+   //
 console.log(capture.pixels.length);
     if (capture.pixels.length > 0) { // don't forget this!
         var total = 0;
         var i = 0;
+
+
+
+
         var savedRandomR = Math.floor(Math.random() * 255) -200;
         // var savedRandomG = Math.floor(Math.random() * 255)-200;
         // var savedRandomB = Math.floor(Math.random() * 255)-150;
 
+
+        for (var WMover=0;WMover<=4*kernel[0];WMover+=4){
+            pixToEdit.push(WMover);
+            for (var HMover=1;HMover<kernel[1];HMover+=1){
+                       pixToEdit.push(WMover + (4*width)*HMover);
+                    }
+        }
+
+
         // for (var L=0;L<capture.pixels.length;L++){
-        //       var redValue = capture.pixels[i];
-        //         total += redValue;
+        //         if(Kcount < kernel[1]){
+        //            pixToEdit.push(i);
+        //            pixToEdit.push(i + (4*w))
+        //         }
+            
+        //    Kcount+=1;
         //         i += 4;
         // } 
-        for (var y = 0; y < h; y++) {
-            for (var x = 0; x < w; x++) {
-          
-                NewPix[i]+= (Math.sin(sinx)*50);
-                  NewPix[i+1]+=Math.sin(sinx)*50;
-                   NewPix[i+2]+=Math.sin(sinx)*50;
+        var rTot=0;
+        var gTot=0;
+        var bTot=0;
 
-                  // capture.pixels[i+3]-=savedRandomB;
+        for (var i =0; i<=kernelSize; i++) {
+            var indexPointer=pixToEdit[i];
+            rTot+=capture.pixels[indexPointer];
+            gTot+=capture.pixels[indexPointer+1];
+            bTot+=capture.pixels[indexPointer+2];
+        }
+        var rAvg = rTot/kernelSize;
+        var gAvg = gTot/kernelSize;
+        var bAvg = bTot/kernelSize;
+
+        console.log(rAvg);
+        for (var i =0; i< pixToEdit.length; i++) {
+            var indexPointer=pixToEdit[i];
+            capture.pixels[indexPointer]=rAvg;
+            capture.pixels[indexPointer+1]=gAvg;
+            capture.pixels[indexPointer+2]=bAvg;
+        }
+
+        // for (var y = 0; y < h; y++) {
+        //     for (var x = 0; x < w; x++) {
+                
+        //         NewPix[i]+= (Math.sin(sinx)*50);
+        //           NewPix[i+1]+=Math.sin(sinx)*50;
+        //            NewPix[i+2]+=Math.sin(sinx)*50;
+
+        //           // capture.pixels[i+3]-=savedRandomB;
   
 
-                // console.log(i);
-                var redValue = capture.pixels[i];
-                total += redValue;
-                i += 4;
-            }
-        }
+        //         // console.log(i);
+        //         var redValue = capture.pixels[i];
+        //         total += redValue;
+        //         i += 4;
+        //     }
+        // }
         sinx+=0.01;
         //
         image(capture, 0, 0, w, h);
         capture.pixels=NewPix;
         capture.updatePixels();
         //console.log(capture.pixels);
-        new Image();   
-        image(capture, 0, 0, w, h);
+   
+         image(capture, 0, 0, w, h);
         //image(originalVideo,0,0,w,h);
         var n = w * h;
         var avg = int(total / n);
