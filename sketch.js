aa8cc89bb5f0e3943811aaaac81a4476ad6f09a4
Asim Satti <aks618@nyu.edu>
2022-11-12 20:21:30 -0800

    MinCraft Effect

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-
+ 
 var originalVideo;
 var pg;
 var kernel=[20,20];
@@ -6,8 +6,8 @@ var kernelSize=kernel[0]*kernel[1];
 var pixToEdit=[];
 var capture;
 var Kcount=0;
-var w = 640;
-var h = 480;
+var w = 960;
+var h = w*0.75;
 var sinx=0.000;
 var savedRandomR = Math.floor(Math.random() * 255);
 var savedRandomG = Math.floor(Math.random() * 255);
@@ -25,13 +25,49 @@ class MatrixObscura{
         this.kernelSize=kernel[0]*kernel[1];
 
         this.pixelDomain=[];
+        this.avgLum;
 
         this.rAvg=0;
         this.gAvg=0;
         this.bvg=0;
 
     }
+    assignRandomPixValues(){
+                for (var i =0; i<=this.kernelSize; i++) {
+            var indexPointer=this.pixelDomain[i]+Mat;
+            rTot+=capture.pixels[indexPointer];
+            gTot+=capture.pixels[indexPointer+1];
+            bTot+=capture.pixels[indexPointer+2];
+        }
+    }
+    PreProcess(){
+        for (var i =0; i<=this.kernelSize; i++) {
+            
+             var indexPointer=this.pixelDomain[i]+this.startI;
+             var lum = 0.2126 * capture.pixels[indexPointer] + 0.7152 *capture.pixels[indexPointer+1] + 0.0722 *capture.pixels[indexPointer+2]
+    
+             if (lum<40){
+                    capture.pixels[indexPointer] *=0.98;
+ 
+                    capture.pixels[indexPointer+1] *= 0.98;
+                    capture.pixels[indexPointer+2] *=0.98;
+             }
+             if(lum>40){
+                   capture.pixels[indexPointer] *=1.03;
+ 
+                    capture.pixels[indexPointer+1] *= 1.03;
+                    capture.pixels[indexPointer+2] *=1.03;
+             }
+            // var rTemp=capture.pixels[indexPointer] 
+            // capture.pixels[indexPointer] =  capture.pixels[indexPointer+1]
+            // capture.pixels[indexPointer+1] = capture.pixels[indexPointer+2]
+            // capture.pixels[indexPointer+2] = 1.2*rTemp;
 
+
+            // Math.Max(capture.pixels[indexPointer],capture.pixels[indexPointer+1],capture.pixels[indexPointer+2])
+
+        }
+    }
     generateRegion(){
           
            for (var WMover=0;WMover<(4*this.kernelWidth);WMover+=4){
@@ -47,16 +83,22 @@ class MatrixObscura{
     }
     GettingAverageFromRegion(){
      
+        
         var rTot=0;
         var gTot=0;
         var bTot=0;
+        var lumTemp=0;
 
         for (var i =0; i<=this.kernelSize; i++) {
             var indexPointer=this.pixelDomain[i]+this.startI;
             rTot+=capture.pixels[indexPointer];
             gTot+=capture.pixels[indexPointer+1];
             bTot+=capture.pixels[indexPointer+2];
+
+            lumTemp+= (0.2126 * capture.pixels[indexPointer] + 0.7152 *capture.pixels[indexPointer+1] + 0.0722 *capture.pixels[indexPointer+2]);
+             
         }
+        this.avgLum=lumTemp/this.kernelSize;
         this.rAvg = rTot/this.kernelSize;
         this.gAvg = gTot/this.kernelSize;
         this.bAvg = bTot/this.kernelSize;
@@ -65,7 +107,7 @@ class MatrixObscura{
         // console.log(rAvg,gAvg,bAvg);
     }
     AssigningNewValues(){
-        
+       // this.PreProcess();
            for (var i =0; i< this.pixelDomain.length; i++) {
                 var indexPointer=this.pixelDomain[i]+this.startI;
                 capture.pixels[indexPointer]=this.rAvg;
@@ -77,7 +119,7 @@ class MatrixObscura{
 
 }
 function setup() {
-    var obscuraDistance=15;
+    var obscuraDistance=40;
     // pg=createGraphics(200,200);
     capture = createCapture({
         audio: false,
@@ -128,12 +170,13 @@ function draw() {
         // console.log(lstMatObsc.length);
         for (var i =0; i<lstMatObsc.length; i++) {
             // console.log("fami nani");
+
             var MatBlock=lstMatObsc[i];
         
-            MatBlock.generateRegion();
-            MatBlock.GettingAverageFromRegion();
-            MatBlock.AssigningNewValues();
-            MatBlock.pixelDomain=[];
+             MatBlock.generateRegion();
+             MatBlock.GettingAverageFromRegion();
+              MatBlock.AssigningNewValues();
+             MatBlock.pixelDomain=[];
 
         }
       
